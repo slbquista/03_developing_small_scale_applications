76bbf3f3abd86ff3cb69ace9e010c9381aff04b3
Finn Turnbull <slbquista@gmail.com>
2018-02-18 17:49:30 +0000

    Changes to date

--- assessment_3/Hearthstone Quiz/Hearthstone Quiz/Form1.Designer.cs
+++ assessment_3/Hearthstone Quiz/Hearthstone Quiz/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Hearthstone_Quiz
 {
-    partial class Form1
+    partial class Index
     {
         /// <summary>
         /// Required designer variable.
@@ -28,193 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label1.Location = new System.Drawing.Point(15, 115);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
-            this.label1.Size = new System.Drawing.Size(500, 50);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Guess the name of each Hearthstone card described below by it\'s flavour text. Goo" +
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Index));
+			this.label1 = new System.Windows.Forms.Label();
+			this.questionField = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.checkAnswer = new System.Windows.Forms.Button();
+			this.nextQuestion = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.scoreField = new System.Windows.Forms.TextBox();
+			this.responseField = new System.Windows.Forms.TextBox();
+			this.resetGame = new System.Windows.Forms.Button();
+			this.answerOne = new System.Windows.Forms.RadioButton();
+			this.answerTwo = new System.Windows.Forms.RadioButton();
+			this.answerThree = new System.Windows.Forms.RadioButton();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.label1.Location = new System.Drawing.Point(15, 115);
+			this.label1.Name = "label1";
+			this.label1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+			this.label1.Size = new System.Drawing.Size(500, 50);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Guess the name of each Hearthstone card described below by it\'s flavour text. Goo" +
     "d luck!";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.textBox1.Location = new System.Drawing.Point(15, 187);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.ShortcutsEnabled = false;
-            this.textBox1.Size = new System.Drawing.Size(500, 75);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TabStop = false;
-            this.textBox1.Text = "What happens when you try and corrupt a corrupted firelord? DOUBLE NEGATIVE, INSE" +
-    "CT!";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(500, 100);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(9, 362);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(250, 25);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Check Answer";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(265, 362);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(250, 25);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Next Question";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label2.Location = new System.Drawing.Point(15, 447);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 25);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Your score is:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.textBox2.Location = new System.Drawing.Point(133, 447);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.ShortcutsEnabled = false;
-            this.textBox2.Size = new System.Drawing.Size(100, 25);
-            this.textBox2.TabIndex = 7;
-            this.textBox2.Text = "1 / 1";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.textBox3.Location = new System.Drawing.Point(15, 404);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.ShortcutsEnabled = false;
-            this.textBox3.Size = new System.Drawing.Size(494, 25);
-            this.textBox3.TabIndex = 8;
-            this.textBox3.Text = "Congradulations, you are correct!";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(15, 493);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(500, 25);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "Reset Game";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.radioButton1.Location = new System.Drawing.Point(15, 268);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(169, 22);
-            this.radioButton1.TabIndex = 10;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Ragnaros the Firelord";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.radioButton2.Location = new System.Drawing.Point(15, 291);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(123, 22);
-            this.radioButton2.TabIndex = 11;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Baron Geddon";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.radioButton3.Location = new System.Drawing.Point(15, 314);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(155, 22);
-            this.radioButton3.TabIndex = 12;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Ragnaros, Lightlord";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 539);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			// 
+			// questionField
+			// 
+			this.questionField.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.questionField.Location = new System.Drawing.Point(15, 187);
+			this.questionField.Multiline = true;
+			this.questionField.Name = "questionField";
+			this.questionField.ReadOnly = true;
+			this.questionField.ShortcutsEnabled = false;
+			this.questionField.Size = new System.Drawing.Size(500, 75);
+			this.questionField.TabIndex = 2;
+			this.questionField.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(500, 100);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// checkAnswer
+			// 
+			this.checkAnswer.Location = new System.Drawing.Point(9, 362);
+			this.checkAnswer.Name = "checkAnswer";
+			this.checkAnswer.Size = new System.Drawing.Size(250, 25);
+			this.checkAnswer.TabIndex = 4;
+			this.checkAnswer.Text = "Check Answer";
+			this.checkAnswer.UseVisualStyleBackColor = true;
+			this.checkAnswer.Click += new System.EventHandler(this.checkAnswer_Click);
+			// 
+			// nextQuestion
+			// 
+			this.nextQuestion.Location = new System.Drawing.Point(265, 362);
+			this.nextQuestion.Name = "nextQuestion";
+			this.nextQuestion.Size = new System.Drawing.Size(250, 25);
+			this.nextQuestion.TabIndex = 5;
+			this.nextQuestion.Text = "Next Question";
+			this.nextQuestion.UseVisualStyleBackColor = true;
+			this.nextQuestion.Click += new System.EventHandler(this.nextQuestion_Click);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.label2.Location = new System.Drawing.Point(15, 447);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 25);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Your score is:";
+			// 
+			// scoreField
+			// 
+			this.scoreField.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.scoreField.Location = new System.Drawing.Point(133, 447);
+			this.scoreField.Multiline = true;
+			this.scoreField.Name = "scoreField";
+			this.scoreField.ReadOnly = true;
+			this.scoreField.ShortcutsEnabled = false;
+			this.scoreField.Size = new System.Drawing.Size(100, 25);
+			this.scoreField.TabIndex = 7;
+			this.scoreField.Text = "1 / 1";
+			// 
+			// responseField
+			// 
+			this.responseField.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.responseField.Location = new System.Drawing.Point(15, 404);
+			this.responseField.Multiline = true;
+			this.responseField.Name = "responseField";
+			this.responseField.ReadOnly = true;
+			this.responseField.ShortcutsEnabled = false;
+			this.responseField.Size = new System.Drawing.Size(494, 25);
+			this.responseField.TabIndex = 8;
+			// 
+			// resetGame
+			// 
+			this.resetGame.Location = new System.Drawing.Point(15, 493);
+			this.resetGame.Name = "resetGame";
+			this.resetGame.Size = new System.Drawing.Size(500, 25);
+			this.resetGame.TabIndex = 9;
+			this.resetGame.Text = "Reset Game";
+			this.resetGame.UseVisualStyleBackColor = true;
+			this.resetGame.Click += new System.EventHandler(this.resetGame_Click);
+			// 
+			// answerOne
+			// 
+			this.answerOne.AutoSize = true;
+			this.answerOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.answerOne.Location = new System.Drawing.Point(15, 268);
+			this.answerOne.Name = "answerOne";
+			this.answerOne.Size = new System.Drawing.Size(14, 13);
+			this.answerOne.TabIndex = 10;
+			this.answerOne.TabStop = true;
+			this.answerOne.UseVisualStyleBackColor = true;
+			this.answerOne.CheckedChanged += new System.EventHandler(this.answerOne_CheckedChanged);
+			// 
+			// answerTwo
+			// 
+			this.answerTwo.AutoSize = true;
+			this.answerTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.answerTwo.Location = new System.Drawing.Point(15, 291);
+			this.answerTwo.Name = "answerTwo";
+			this.answerTwo.Size = new System.Drawing.Size(14, 13);
+			this.answerTwo.TabIndex = 11;
+			this.answerTwo.TabStop = true;
+			this.answerTwo.UseVisualStyleBackColor = true;
+			this.answerTwo.CheckedChanged += new System.EventHandler(this.answerTwo_CheckedChanged);
+			// 
+			// answerThree
+			// 
+			this.answerThree.AutoSize = true;
+			this.answerThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.answerThree.Location = new System.Drawing.Point(15, 314);
+			this.answerThree.Name = "answerThree";
+			this.answerThree.Size = new System.Drawing.Size(14, 13);
+			this.answerThree.TabIndex = 12;
+			this.answerThree.TabStop = true;
+			this.answerThree.UseVisualStyleBackColor = true;
+			this.answerThree.CheckedChanged += new System.EventHandler(this.answerThree_CheckedChanged);
+			// 
+			// Index
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(531, 539);
+			this.Controls.Add(this.answerThree);
+			this.Controls.Add(this.answerTwo);
+			this.Controls.Add(this.answerOne);
+			this.Controls.Add(this.resetGame);
+			this.Controls.Add(this.responseField);
+			this.Controls.Add(this.scoreField);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.nextQuestion);
+			this.Controls.Add(this.checkAnswer);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.questionField);
+			this.Controls.Add(this.label1);
+			this.Name = "Index";
+			this.Text = "Index";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox questionField;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button checkAnswer;
+        private System.Windows.Forms.Button nextQuestion;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox scoreField;
+        private System.Windows.Forms.TextBox responseField;
+        private System.Windows.Forms.Button resetGame;
+        private System.Windows.Forms.RadioButton answerOne;
+        private System.Windows.Forms.RadioButton answerTwo;
+        private System.Windows.Forms.RadioButton answerThree;
     }
 }
 
--- assessment_3/Hearthstone Quiz/Hearthstone Quiz/Form1.cs
+++ assessment_3/Hearthstone Quiz/Hearthstone Quiz/Form1.cs
@@ -8,23 +8,131 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Hearthstone_Quiz
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace Hearthstone_Quiz {
+
+    public partial class Index : Form { //This class encapsulates everything
+
+		//Declare and initialise variables to be used in app.
+        string[,] questionsAndAnswers = new string[10, 5] { //2D Array containing the questions and answers
+            { "'Hey! Ancestors!'", "Ancestor’s call", "Rhonin", "Living Roots", "Ancestor’s call"},
+			{ "Sometimes the Old Gods’ corruptions gives you power untold, sometimes you get +1 Attack. We can’t all be winners in the Eldritch lottery.", "Sky Cap’n Kragg", "Twisted Worgen", "Drakonid Operative", "Twisted Worgen"},
+			{ "Or if you’re too tired, you can just kind of lean against the darkness.", "Acidmaw", "Dreadscale", "Stand Against Darkness", "Stand Against Darkness"},
+			{ "peerc rewop", "Big Game Hunter", "Aya Blackpaw", "Am’Gam Rager", "Am’Gam Rager"},
+			{ "What happens when you try and corrupt a corrupted firelord? DOUBLE NEGATIVE, INSECT!", "Skulking Geist", "Ragnaros, Lightlord", "Lilian Voss", "Ragnaros, Lightlord"},
+			{ "Clever girl!", "Mounted Raptor", "Ysera", "C'Thun", "Mounted Raptor"},
+			{ "'Oh, I’m not serious about it. I only pirate on the weekends.'", "Small-Time Buccaneer", "Wrathion", "White Eyes", "Small-Time Buccaneer"},
+			{ "It has double the Attack. 'Go on.' And double the Health. 'Yes??' And its trigger has twice the effect. 'WHAT???' And it costs… Five times more.", "Prince Keleseth", "Red Mana Wyrm", "The Lich King", "Red Mana Wyrm"},
+			{ "Meditating under a jade blossom is said to grant you wisdom, unless you have a pollen allergy.", "Frostmourne", "Aviana", "Jade Blossom", "Jade Blossom"},
+			{ "Now we know why they wear tiny watches.", "Raza the Chained", "Profesor Putricide", "Small-Time Recruits", "Small-Time Recruits"}
+        };
+		int questionCounter = 0;
+		String givenAnswer = "";
+        String trueAnswer = "Congradulations, you are correct!";                         //Text 1 for responseField
+        String falseAnswer = "Commiserations, you are incorrect!";                       //Text 2 for responseField
+        int correctAnswers = 0;                                                          //Number of correctly answered questions
+        int answeredQuestions = 1;                                                       //Number of questions answered
+
+
+        public Index() { //Runs on startup
+            //Calls the method that initializes the page
             InitializeComponent();
+
+            //Fills initial data into fields
+            questionField.Text = questionsAndAnswers[questionCounter, 0];
+            answerOne.Text = questionsAndAnswers[questionCounter, 1];
+            answerTwo.Text = questionsAndAnswers[questionCounter, 2];
+            answerThree.Text = questionsAndAnswers[questionCounter, 3];
+            scoreField.Text = correctAnswers + " / " + answeredQuestions;
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+		private void answerOne_CheckedChanged(object sender, EventArgs e) { //Sets given answer to option 1
+			givenAnswer = questionsAndAnswers[questionCounter, 1];
+		}
 
-        }
+		private void answerTwo_CheckedChanged(object sender, EventArgs e) { //Sets given answer to option 2
+			givenAnswer = questionsAndAnswers[questionCounter, 2];
+		}
 
-        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
+		private void answerThree_CheckedChanged(object sender, EventArgs e) { //Sets given answer to option 3
+			givenAnswer = questionsAndAnswers[questionCounter, 3];
+		}
 
-        }
-    }
+		private void checkAnswer_Click(object sender, EventArgs e) { //Checks the answer
+			if (givenAnswer.Equals(questionsAndAnswers[questionCounter, 4], StringComparison.Ordinal)) { //If given answer is equal to correct answer then
+				//Add 1 to correct answers
+				correctAnswers++; 
+
+				//Ouput correct response
+				responseField.Text = trueAnswer;
+
+			}
+			else {
+				//Output false answer
+				responseField.Text = falseAnswer;
+			}
+
+			//Ouput score regardless of answer
+			scoreField.Text = correctAnswers + " / " + answeredQuestions;
+
+			//Disables radio buttons and checkAnswer once the answer has been checked
+			answerOne.Enabled = false;
+			answerTwo.Enabled = false;
+			answerThree.Enabled = false;
+			checkAnswer.Enabled = false;
+		}
+
+		private void nextQuestion_Click(object sender, EventArgs e) {
+			if (questionCounter != (questionsAndAnswers.GetLength(0) - 1)) {
+				questionCounter++;
+				answeredQuestions++;
+
+				questionField.Text = questionsAndAnswers[questionCounter, 0];
+				answerOne.Text = questionsAndAnswers[questionCounter, 1];
+				answerTwo.Text = questionsAndAnswers[questionCounter, 2];
+				answerThree.Text = questionsAndAnswers[questionCounter, 3];
+				answerOne.Checked = false;
+				answerTwo.Checked = false;
+				answerThree.Checked = false;
+				answerOne.Enabled = true;
+				answerTwo.Enabled = true;
+				answerThree.Enabled = true;
+				checkAnswer.Enabled = true;
+				scoreField.Text = correctAnswers + " / " + answeredQuestions;
+			} else {
+				questionField.Text = "You have completed the quiz.";
+				answerOne.Text = "";
+				answerTwo.Text = "";
+				answerThree.Text = "";
+				answerOne.Enabled = false;
+				answerTwo.Enabled = false;
+				answerThree.Enabled = false;
+				checkAnswer.Enabled = false;
+				nextQuestion.Enabled = false;
+				responseField.Text = "To start afresh select 'Reset Game'";
+			}
+
+		}
+
+		private void resetGame_Click(object sender, EventArgs e) { //Reset everything!
+			questionCounter = 0;
+			givenAnswer = "";
+			correctAnswers = 0;
+			answeredQuestions = 0;
+
+			questionField.Text = questionsAndAnswers[questionCounter, 0];
+			answerOne.Text = questionsAndAnswers[questionCounter, 1];
+			answerTwo.Text = questionsAndAnswers[questionCounter, 2];
+			answerThree.Text = questionsAndAnswers[questionCounter, 3];
+			answerOne.Checked = false;
+			answerTwo.Checked = false;
+			answerThree.Checked = false;
+			answerOne.Enabled = true;
+			answerTwo.Enabled = true;
+			answerThree.Enabled = true;
+			checkAnswer.Enabled = true;
+			nextQuestion.Enabled = true;
+			responseField.Text = "";
+			scoreField.Text = correctAnswers + " / " + answeredQuestions;
+		}
+	}
 }
